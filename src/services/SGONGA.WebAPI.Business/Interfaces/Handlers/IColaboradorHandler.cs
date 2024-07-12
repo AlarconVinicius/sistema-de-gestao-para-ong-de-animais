586ea595cfdd757697a5005bfa15c9df26ec06e1
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 20:46:25 -0300

    feat: Adicionar interfaces de Handlers

---
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IColaboradorHandler.cs
@@ -0,0 +1,14 @@
+﻿using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Requests;
+using SGONGA.WebAPI.Business.Responses;
+
+namespace SGONGA.WebAPI.Business.Interfaces.Handlers;
+
+public interface IColaboradorHandler
+{
+    Task CreateAsync(CreateColaboradorRequest request);
+    Task UpdateAsync(UpdateColaboradorRequest request);
+    Task DeleteAsync(DeleteColaboradorRequest request);
+    Task<ColaboradorResponse> GetByIdAsync(GetColaboradorByIdRequest request);
+    Task<PagedResult<ColaboradorResponse>> GetAllAsync(GetAllColaboradoresRequest request);
+}
---
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IONGHandler.cs
@@ -0,0 +1,14 @@
+﻿using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Requests;
+using SGONGA.WebAPI.Business.Responses;
+
+namespace SGONGA.WebAPI.Business.Interfaces.Handlers;
+
+public interface IONGHandler
+{
+    Task CreateAsync(CreateONGRequest request);
+    Task UpdateAsync(UpdateONGRequest request);
+    Task DeleteAsync(DeleteONGRequest request);
+    Task<ONGResponse> GetByIdAsync(GetONGByIdRequest request);
+    Task<PagedResult<ONGResponse>> GetAllAsync(GetAllONGsRequest request);
+}
