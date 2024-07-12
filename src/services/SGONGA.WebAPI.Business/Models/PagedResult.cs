03dd5efce3566c262e3adc085dedb2c9287307ed
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 20:44:46 -0300

    feat: Adicionar PagedRequest

---
+++ src/services/SGONGA.WebAPI.Business/Models/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace SGONGA.WebAPI.Business.Models;
+
+public class PagedResult<T> where T : class
+{
+    public IEnumerable<T> List { get; set; } = Enumerable.Empty<T>();
+    public int TotalResults { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public string? Query { get; set; }
+    public bool ReturnAll { get; set; }
+}
