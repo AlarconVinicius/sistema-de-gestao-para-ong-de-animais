7d41b762a02f10b178050081ac809b1a5aac0470
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 20:46:03 -0300

    feat: Adicionar Responses

---
+++ src/services/SGONGA.WebAPI.Business/Responses/ColaboradorResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SGONGA.WebAPI.Business.Responses;
+
+public class ColaboradorResponse : Response
+{
+    [DisplayName("Id")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;
+
+    [DisplayName("Data de Cadastro")]
+    public DateTime CreatedAt { get; set; }
+
+    [DisplayName("Data de Modificação")]
+    public DateTime UpdatedAt { get; set; }
+
+    public ColaboradorResponse() { }
+
+    public ColaboradorResponse(Guid id, string email, DateTime createdAt, DateTime updatedAt)
+    {
+        Id = id;
+        Email = email;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+}
