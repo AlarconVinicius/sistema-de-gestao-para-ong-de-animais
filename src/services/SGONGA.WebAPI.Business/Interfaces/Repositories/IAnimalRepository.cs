a37da8c6c545aaa9123a3f08c574397abb8d605b
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 23:32:17 -0300

    feat: Adicionar sobrecarga para GetByIdAsync

--- src/services/SGONGA.WebAPI.Business/Interfaces/Repositories/IAnimalRepository.cs
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Repositories/IAnimalRepository.cs
@@ -5,5 +5,6 @@ namespace SGONGA.WebAPI.Business.Interfaces.Repositories;
 
 public interface IAnimalRepository : IRepository<Animal>
 {
+    Task<Animal> GetByIdAsync(Guid id, Guid tenantId);
     new Task<PagedResult<Animal>> GetAllPagedAsync(Expression<Func<Animal, bool>>? predicate = null, int page = 1, int pageSize = 10, string? query = null, bool returnAll = false);
 }
--- src/services/SGONGA.WebAPI.Business/Interfaces/Repositories/IColaboradorRepository.cs
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Repositories/IColaboradorRepository.cs
@@ -4,4 +4,5 @@ namespace SGONGA.WebAPI.Business.Interfaces.Repositories;
 
 public interface IColaboradorRepository : IRepository<Colaborador>
 {
+    Task<Colaborador> GetByIdAsync(Guid id, Guid tenantId);
 }
