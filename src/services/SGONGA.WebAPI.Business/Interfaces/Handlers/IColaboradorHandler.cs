28c4c2cbd8ae99588f575c0e9d45e7491ee52293
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 23:33:05 -0300

    fix: Substituir o tipo de retorno do GetAllAsync

--- src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IColaboradorHandler.cs
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IColaboradorHandler.cs
@@ -1,5 +1,4 @@
-﻿using SGONGA.WebAPI.Business.Models;
-using SGONGA.WebAPI.Business.Requests;
+﻿using SGONGA.WebAPI.Business.Requests;
 using SGONGA.WebAPI.Business.Responses;
 
 namespace SGONGA.WebAPI.Business.Interfaces.Handlers;
@@ -10,5 +9,5 @@ public interface IColaboradorHandler
     Task UpdateAsync(UpdateColaboradorRequest request);
     Task DeleteAsync(DeleteColaboradorRequest request);
     Task<ColaboradorResponse> GetByIdAsync(GetColaboradorByIdRequest request);
-    Task<PagedResult<ColaboradorResponse>> GetAllAsync(GetAllColaboradoresRequest request);
+    Task<PagedResponse<ColaboradorResponse>> GetAllAsync(GetAllColaboradoresRequest request);
 }
