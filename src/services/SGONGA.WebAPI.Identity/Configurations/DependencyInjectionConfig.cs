a70f9ccf7755ae28a2717da78b63696026aeeecd
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-13 20:47:08 -0300

    feat: Adicionar DependencyInjectionConfig

---
+++ src/services/SGONGA.WebAPI.Identity/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SGONGA.Core.Configurations;
+using SGONGA.WebAPI.Business.Interfaces.Handlers;
+using SGONGA.WebAPI.Identity.Context;
+using SGONGA.WebAPI.Identity.Extensions;
+using SGONGA.WebAPI.Identity.Handlers;
+using System.Text;
+
+namespace SGONGA.WebAPI.Identity.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection RegisterIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ??
+                throw new InvalidOperationException("Connection String is not found")));
+
+        services.AddIdentityConfig()
+                .AddJwtConfig(configuration);
+
+        services.AddScoped<IIdentityHandler, IdentityHandler>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddIdentityConfig(this IServiceCollection services)
+    {
+        services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+        });
+        return services;
+    }
+    private static IServiceCollection AddJwtConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var appSettingsSection = configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings!.Secret);
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = true;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.Audience,
+                ValidIssuer = appSettings.Issuer
+            };
+        });
+
+        return services;
+    }
+}
