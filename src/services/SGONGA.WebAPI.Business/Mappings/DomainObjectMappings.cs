d737ba2fd73b1dd000e5af5769a1348ca3310086
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 22:49:34 -0300

    feat: Adicionar Mappings

---
+++ src/services/SGONGA.WebAPI.Business/Mappings/DomainObjectMappings.cs
@@ -0,0 +1,17 @@
+﻿using SGONGA.WebAPI.Business.Models.DomainObjects;
+using SGONGA.WebAPI.Business.Responses;
+
+namespace SGONGA.WebAPI.Business.Mappings;
+
+public static class DomainObjectMappings
+{
+    public static EnderecoResponse MapDomainToResponse(this Endereco request)
+    {
+        if (request == null)
+        {
+            return null!;
+        }
+
+        return new EnderecoResponse(request.Rua, request.Cidade, request.Estado, request.CEP, request.Complemento);
+    }
+}
