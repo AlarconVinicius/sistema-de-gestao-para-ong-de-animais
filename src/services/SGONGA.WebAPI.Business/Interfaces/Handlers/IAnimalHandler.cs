56f17d0983d5f138b37eb5a7d04dc51c6273cf7e
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 23:53:58 -0300

    fix: Corrigir tipo de retorno do GetAllAsync

--- src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IAnimalHandler.cs
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IAnimalHandler.cs
@@ -1,5 +1,4 @@
-﻿using SGONGA.WebAPI.Business.Models;
-using SGONGA.WebAPI.Business.Requests;
+﻿using SGONGA.WebAPI.Business.Requests;
 using SGONGA.WebAPI.Business.Responses;
 
 namespace SGONGA.WebAPI.Business.Interfaces.Handlers;
@@ -10,5 +9,5 @@ public interface IAnimalHandler
     Task UpdateAsync(UpdateAnimalRequest request);
     Task DeleteAsync(DeleteAnimalRequest request);
     Task<AnimalResponse> GetByIdAsync(GetAnimalByIdRequest request);
-    Task<PagedResult<AnimalResponse>> GetAllAsync(GetAllAnimaisRequest request);
+    Task<PagedResponse<AnimalResponse>> GetAllAsync(GetAllAnimaisRequest request);
 }
