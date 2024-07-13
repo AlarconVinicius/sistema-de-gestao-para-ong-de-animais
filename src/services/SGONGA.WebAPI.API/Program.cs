4c770405c3aa166a24f2f62fdd32846f5fc34c0b
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-13 20:56:21 -0300

    feat: Configurar projeto Identity

--- src/services/SGONGA.WebAPI.API/Program.cs
+++ src/services/SGONGA.WebAPI.API/Program.cs
@@ -1,11 +1,15 @@
+using SGONGA.Core.Configurations;
 using SGONGA.WebAPI.Data.Configurations;
+using SGONGA.WebAPI.Identity.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.RegisterDataServices(builder.Configuration);
+builder.Services.RegisterCoreServices()
+                .RegisterDataServices(builder.Configuration)
+                .RegisterIdentityServices(builder.Configuration);
 
 builder.Services.AddSwaggerGen();
 
