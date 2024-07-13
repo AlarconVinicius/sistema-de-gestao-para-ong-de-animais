bee9a7fd590bfecf782da8cf11ba98cc9d663804
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 21:37:15 -0300

    feat: Adicionar BaseHandler

---
+++ src/services/SGONGA.WebAPI.Business/Handlers/BaseHandler.cs
@@ -0,0 +1,75 @@
+﻿using SGONGA.Core.Notifications;
+using SGONGA.Core.User;
+using SGONGA.WebAPI.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace SGONGA.WebAPI.Business.Handlers;
+
+public class BaseHandler
+{
+    private readonly INotifier _notifier;
+    protected ValidationResult _validationResult;
+    public readonly IAspNetUser AppUser;
+    protected Guid UserId { get; set; }
+    protected Guid TenantId = Guid.Empty;
+    protected bool IsUserAuthenticated { get; set; }
+
+    protected BaseHandler(INotifier notifier, IAspNetUser appUser)
+    {
+        _notifier = notifier;
+        _validationResult = new ValidationResult();
+
+        AppUser = appUser;
+
+        if (appUser.IsAuthenticated())
+        {
+            UserId = appUser.GetUserId();
+            IsUserAuthenticated = true;
+            TenantId = appUser.GetTenantId() != Guid.Empty ? appUser.GetTenantId() : Guid.Empty;
+        }
+    }
+
+    protected void Notify(ValidationResult validationResult)
+    {
+        foreach (var error in validationResult.Errors)
+        {
+            Notify(error.ErrorMessage);
+        }
+    }
+
+    protected void Notify(string message)
+    {
+        _notifier.Handle(new Notification(message));
+        _validationResult.Errors.Add(new ValidationFailure(string.Empty, message));
+    }
+    protected bool IsOperationValid()
+    {
+        return !_notifier.HasNotification();
+    }
+    protected void ClearNotifications()
+    {
+        _notifier.Clear();
+        return;
+    }
+    protected bool ExecuteValidation<TV, TE>(TV validation, TE entity) where TV : AbstractValidator<TE> where TE : Entity
+    {
+        var validator = validation.Validate(entity);
+
+        if (validator.IsValid) return true;
+
+        Notify(validator);
+
+        return false;
+    }
+
+    protected bool TenantIsEmpty()
+    {
+        if (TenantId == Guid.Empty)
+        {
+            Notify("TenantId não encontrado.");
+            return true;
+        }
+        return false;
+    }
+}
