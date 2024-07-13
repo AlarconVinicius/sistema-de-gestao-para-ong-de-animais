e2c6661d210a61dc1b691d0c193e359ef265607d
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-12 21:45:46 -0300

    feat: Adicionar PagedResponse

--- src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IONGHandler.cs
+++ src/services/SGONGA.WebAPI.Business/Interfaces/Handlers/IONGHandler.cs
@@ -1,5 +1,4 @@
-﻿using SGONGA.WebAPI.Business.Models;
-using SGONGA.WebAPI.Business.Requests;
+﻿using SGONGA.WebAPI.Business.Requests;
 using SGONGA.WebAPI.Business.Responses;
 
 namespace SGONGA.WebAPI.Business.Interfaces.Handlers;
@@ -10,5 +9,5 @@ public interface IONGHandler
     Task UpdateAsync(UpdateONGRequest request);
     Task DeleteAsync(DeleteONGRequest request);
     Task<ONGResponse> GetByIdAsync(GetONGByIdRequest request);
-    Task<PagedResult<ONGResponse>> GetAllAsync(GetAllONGsRequest request);
+    Task<PagedResponse<ONGResponse>> GetAllAsync(GetAllONGsRequest request);
 }
