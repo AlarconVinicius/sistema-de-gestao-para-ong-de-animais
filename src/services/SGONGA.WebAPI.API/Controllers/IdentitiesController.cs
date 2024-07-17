1b9ade1acfd0217db00cc4e50f10c721182ef9b8
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-17 18:23:43 -0300

    feat: Adicionar IdentitiesController

---
+++ src/services/SGONGA.WebAPI.API/Controllers/IdentitiesController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SGONGA.Core.Configurations;
+using SGONGA.Core.Extensions;
+using SGONGA.Core.Notifications;
+using SGONGA.WebAPI.API.Controllers.Shared;
+using SGONGA.WebAPI.Business.Interfaces.Handlers;
+using SGONGA.WebAPI.Business.Requests;
+
+namespace SGONGA.WebAPI.API.Controllers;
+
+[Authorize]
+[Route("api/v1/identities/")]
+public class IdentitiesController : ApiController
+{
+    public readonly IIdentityHandler _identityHandler;
+    public IdentitiesController(INotifier notifier, IIdentityHandler identityHandler) : base(notifier)
+    {
+        _identityHandler = identityHandler;
+    }
+
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUserRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+
+        var response = await _identityHandler.LoginAsync(request);
+
+        return IsOperationValid() ? ResponseOk(response) : ResponseBadRequest();
+    }
+
+    [ClaimsAuthorize("Permissions", "SuperAdmin")]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(CreateUserRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+        var response = await _identityHandler.CreateAsync(request);
+
+        return IsOperationValid() ? ResponseOk(response) : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPut("update/email")]
+    public async Task<IActionResult> UpdateUserEmail(UpdateUserEmailRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+        await _identityHandler.UpdateEmailAsync(request);
+
+        return IsOperationValid() ? ResponseOk() : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPut("update/password")]
+    public async Task<IActionResult> UpdateUserPassword(UpdateUserPasswordRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+        await _identityHandler.UpdatePasswordAsync(request);
+
+        return IsOperationValid() ? ResponseOk() : ResponseBadRequest();
+    }
+
+    [ClaimsAuthorize("Permissions", "SuperAdmin")]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        DeleteUserRequest request = new(id);
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+        await _identityHandler.DeleteAsync(request);
+
+        return IsOperationValid() ? ResponseOk() : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        GetUserByIdRequest request = new(id);
+        var result = await _identityHandler.GetByIdAsync(request);
+
+        return IsOperationValid() ? ResponseOk(result) : ResponseBadRequest();
+
+    }
+
+    [ClaimsAuthorize("Permissions", "SuperAdmin")]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null)
+    {
+        GetAllUsersRequest request = new()
+        {
+            PageSize = ps,
+            PageNumber = page,
+            Query = q
+        };
+        var result = await _identityHandler.GetAllAsync(request);
+
+        return IsOperationValid() ? ResponseOk(result) : ResponseBadRequest();
+    }
+}
