ae438862df0abf9e811d80893cdbabf465730340
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-07-17 21:04:34 -0300

    feat: Adicionar AnimaisController

---
+++ src/services/SGONGA.WebAPI.API/Controllers/AnimaisController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SGONGA.Core.Configurations;
+using SGONGA.Core.Notifications;
+using SGONGA.WebAPI.API.Controllers.Shared;
+using SGONGA.WebAPI.Business.Interfaces.Handlers;
+using SGONGA.WebAPI.Business.Requests;
+
+namespace SGONGA.WebAPI.API.Controllers;
+
+[Authorize]
+[Route("api/v1/animais/")]
+public class AnimaisController : ApiController
+{
+    public readonly IAnimalHandler _animalHandler;
+    public AnimaisController(INotifier notifier, IAnimalHandler animalHandler) : base(notifier)
+    {
+        _animalHandler = animalHandler;
+    }
+
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id, bool tenantFiltro = false)
+    {
+        GetAnimalByIdRequest request = new(id, tenantFiltro);
+        var result = await _animalHandler.GetByIdAsync(request);
+
+        return IsOperationValid() ? ResponseOk(result) : ResponseBadRequest();
+
+    }
+
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, bool tenantFiltro = false)
+    {
+        GetAllAnimaisRequest request = new()
+        {
+            PageSize = ps,
+            PageNumber = page,
+            Query = q,
+            TenantFiltro = tenantFiltro
+        };
+        var result = await _animalHandler.GetAllAsync(request);
+
+        return IsOperationValid() ? ResponseOk(result) : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateAnimalRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+        await _animalHandler.CreateAsync(request);
+
+        return IsOperationValid() ? ResponseCreated() : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, UpdateAnimalRequest request)
+    {
+        if (id != request.Id) return ResponseBadRequest("Os IDs não correspondem.");
+        if (!ModelState.IsValid)
+        {
+            return ResponseBadRequest(ModelState);
+        }
+
+        await _animalHandler.UpdateAsync(request);
+
+        return IsOperationValid() ? ResponseOk() : ResponseBadRequest();
+    }
+
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomResult), StatusCodes.Status400BadRequest)]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _animalHandler.DeleteAsync(new DeleteAnimalRequest(id));
+
+        return IsOperationValid() ? ResponseOk() : ResponseBadRequest();
+    }
+}
