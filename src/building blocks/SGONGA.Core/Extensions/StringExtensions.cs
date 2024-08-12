32edaf3f5bfca7ef84a107adca7c35e1bab9957f
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-08-12 15:00:02 -0300

    feat: Adicionar StringExtensions

---
+++ src/building blocks/SGONGA.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SGONGA.Core.Extensions;
+public static class StringExtensions
+{
+    public static Guid? TryParseGuid(this string? input)
+    {
+        if (Guid.TryParse(input, out Guid guid))
+        {
+            return guid;
+        }
+        return null;
+    }
+}
