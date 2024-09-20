7b8b30b5bc10aa19ce0286b20dd02debe17c53d2
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-09-20 17:19:48 -0300

    feat: Adicionar ValidationErrors

---
+++ src/services/SGONGA.WebAPI.Business/Errors/ValidationErrors.cs
@@ -0,0 +1,10 @@
+﻿using SGONGA.WebAPI.Business.Abstractions;
+
+namespace SGONGA.WebAPI.Business.Errors;
+
+public static class ValidationErrors
+{
+    public static Error EmailEmUso(string email) => Error.Conflict("EMAIL_EM_USO", $"O email = '{email}' já está em uso.");
+    public static Error ApelidoEmUso(string apelido) => Error.Conflict("APELIDO_EM_USO", $"O apelido = '{apelido}' já está em uso.");
+    public static Error DocumentoEmUso(string documento) => Error.Conflict("DOCUMENTO_EM_USO", $"O documento = '{documento}' já está em uso.");
+}
