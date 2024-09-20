7f73102689548df39095433ef1b4281c4228fe8a
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-09-20 07:53:34 -0300

    feat: Padronizar retornos de falha da API
    - Cria o método ToProblemDetails para mapear os resultados de falha para o tipo Problem

---
+++ src/services/SGONGA.WebAPI.API/Extensions/ApiResultExtensions.cs
@@ -0,0 +1,61 @@
+﻿using SGONGA.WebAPI.Business.Abstractions;
+
+namespace SGONGA.WebAPI.API.Extensions;
+
+public static class ApiResultExtensions
+{
+    public static IResult ToProblemDetails(this Result result)
+    {
+        if (result.IsSuccess)
+        {
+            throw new InvalidOperationException("Invalid error");
+        }
+        return Results.Problem(
+            statusCode: GetStatusCode(result.Error.Type),
+            title: GetTitle(result.Error.Type),
+            type: GetType(result.Error.Type),
+            extensions: GetErrors(result)
+        );
+
+        static int GetStatusCode(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Valdiation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+        static string GetTitle(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Valdiation => "Bad Request",
+                ErrorType.NotFound => "Not Found",
+                ErrorType.Conflict => "Conflict",
+                _ => "Server Failure"
+            };
+
+        static string GetType(ErrorType statusCode) =>
+            statusCode switch
+            {
+                ErrorType.Valdiation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+                _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+            };
+
+        static Dictionary<string, object?>? GetErrors(Result result)
+        {
+            //if (result.Error is not ValidationError validationError)
+            //{
+            //    return null;
+            //}
+
+            return new Dictionary<string, object?>
+            {
+                //{ "errors", validationError.Errors },
+                { "errors", new[]  { result.Error } }
+            };
+        }
+    }
+}
