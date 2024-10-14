776e3c4fbabf3a160808fe538334b5608d5f82d0
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-10-14 09:26:13 -0300

    feat: Adicionar classe RequestValidator
    #18

---
+++ src/services/SGONGA.WebAPI.Business/Abstractions/RequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace SGONGA.WebAPI.Business.Abstractions;
+public static class RequestValidator
+{
+    public static Result IsValid<TRequest>(TRequest request, AbstractValidator<TRequest> validator)
+    {
+        if (request == null)
+        {
+            return Error.Failure("VALIDATION_ERRORS", "Os dados informados são inválidos");
+        }
+
+        ValidationResult validationResult = validator.Validate(request);
+        if (!validationResult.IsValid)
+        {
+            return validationResult.Errors.Select(x => Error.Validation("VALIDATION_ERRORS", x.ErrorMessage)).ToList();
+        }
+
+        return Result.Ok();
+    }
+}
