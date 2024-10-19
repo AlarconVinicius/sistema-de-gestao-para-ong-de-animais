59802442bd82864a793b13b0b3a1c5ad17e87ff1
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-10-19 13:06:03 -0300

    feat: Adicionar BasePagedQuery e BasePagedResponse
    #18

---
+++ src/services/SGONGA.WebAPI.API/Shared/BasePagedQuery.cs
@@ -0,0 +1,21 @@
+﻿using SGONGA.WebAPI.API.Abstractions.Messaging;
+
+namespace SGONGA.WebAPI.API.Shared;
+
+public class BasePagedQuery<TResponse> : IQuery<TResponse>
+{
+    public int PageSize { get; set; } = 50;
+    public int PageNumber { get; set; } = 1;
+    public string? Query { get; set; } = null;
+    public bool ReturnAll { get; set; }
+
+    public BasePagedQuery() { }
+
+    protected BasePagedQuery(int pageSize, int pageNumber, string? query, bool returnAll)
+    {
+        PageSize = pageSize;
+        PageNumber = pageNumber;
+        Query = query;
+        ReturnAll = returnAll;
+    }
+}
---
+++ src/services/SGONGA.WebAPI.API/Shared/BasePagedResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SGONGA.WebAPI.API.Shared;
+
+public class BasePagedResponse<TData>
+{
+    public IEnumerable<TData> List { get; set; } = new List<TData>();
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalResults { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalResults / (double)PageSize);
+    public string? Query { get; set; }
+    public bool ReturnAll { get; set; }
+
+    public BasePagedResponse() { }
+
+    [JsonConstructor]
+    public BasePagedResponse(IEnumerable<TData> list, int totalResults, int pageIndex = 1, int pageSize = 50, string? query = null, bool returnAll = false)
+    {
+        List = list;
+        TotalResults = totalResults;
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        Query = query;
+        ReturnAll = returnAll;
+    }
+
+}
