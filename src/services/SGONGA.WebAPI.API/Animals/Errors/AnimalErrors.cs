c85fb18ed82aae5f715f57b9a74448e0422a6b3e
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-10-20 08:01:03 -0300

    refactor: Atualizar mensagem de erro de AnimalErrors
    #18

--- src/services/SGONGA.WebAPI.API/Animals/Errors/AnimalErrors.cs
+++ src/services/SGONGA.WebAPI.API/Animals/Errors/AnimalErrors.cs
@@ -4,6 +4,6 @@ namespace SGONGA.WebAPI.API.Animals.Errors;
 
 public static class AnimalErrors
 {
-    public static Error AnimalNotFound(Guid id) => Error.NotFound("ANIMAL_NOT_FOUND", $"The animal with Id = '{id}' was not found.");
-    public static readonly Error NoAnimalsFound = Error.NotFound("NO_ANIMALS_FOUND", "No animals found.");
+    public static Error AnimalNotFound(Guid id) => Error.NotFound("ANIMAL_NOT_FOUND", $"O animal com Id = '{id}' não foi encontrado.");
+    public static readonly Error NoAnimalsFound = Error.NotFound("NO_ANIMALS_FOUND", "Nenhum animal encontrado.");
 }
