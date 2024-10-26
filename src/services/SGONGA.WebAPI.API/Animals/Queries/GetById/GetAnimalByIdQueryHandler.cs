0b47e8e9769af64c62de8b29a3e06eb6f0a18b6e
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-10-26 10:43:37 -0300

    refactor: Adicionar retorno de erro quando animal for null
    #18

--- src/services/SGONGA.WebAPI.API/Animals/Queries/GetById/GetAnimalByIdQueryHandler.cs
+++ src/services/SGONGA.WebAPI.API/Animals/Queries/GetById/GetAnimalByIdQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.API.Animals.Errors;
 using SGONGA.WebAPI.Business.Abstractions;
 using SGONGA.WebAPI.Business.Animals.Interfaces.Repositories;
 using SGONGA.WebAPI.Business.Animals.Responses;
@@ -10,14 +11,22 @@ internal sealed class GetAnimalByIdQueryHandler(IAnimalRepository AnimalReposito
 {
     public async Task<Result<AnimalResponse>> Handle(GetAnimalByIdQuery query, CancellationToken cancellationToken)
     {
+        AnimalResponse result;
         if (query.TenantFiltro)
         {
             Result<Guid> tenantId = await TenantProvider.GetTenantId();
             if (tenantId.IsFailed)
                 return tenantId.Errors;
-
-            return await AnimalRepository.GetByIdAsync(query.Id, tenantId.Value);
+            result = await AnimalRepository.GetByIdAsync(query.Id, tenantId.Value);
         }
-        return await AnimalRepository.GetByIdAsync(query.Id, null);
+        else 
+        {
+            result = await AnimalRepository.GetByIdAsync(query.Id, null);
+        }
+
+        if(result is null)
+            return AnimalErrors.AnimalNotFound(query.Id);
+
+        return result;
     }
 }
