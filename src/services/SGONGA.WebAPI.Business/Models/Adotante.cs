a6c8002ef8af8d60046a18e9a8ed80df429d6a97
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:28:36 -0300

    feat: Adicionar método create e update em Adotante e ONG
    #18

--- src/services/SGONGA.WebAPI.Business/Models/Adotante.cs
+++ src/services/SGONGA.WebAPI.Business/Models/Adotante.cs
@@ -1,11 +1,68 @@
-﻿using SGONGA.WebAPI.Business.Models.DomainObjects;
+﻿using Microsoft.AspNetCore.Routing;
+using SGONGA.WebAPI.Business.Models.DomainObjects;
+using System.Runtime.ConstrainedExecution;
 
 namespace SGONGA.WebAPI.Business.Models;
 public sealed class Adotante : Usuario
 {
-    public Adotante() { }
+    private Adotante() { }
+    private Adotante(Guid id) : base(id) { }
 
     public Adotante(Guid id, Guid tenantId, string nome, string apelido, string documento, string site, Contato contato, bool telefoneVisivel, bool assinarNewsletter, DateTime dataNascimento, string estado, string cidade, string? sobre) : base(id, tenantId, EUsuarioTipo.Adotante, nome, apelido, documento, site, contato, telefoneVisivel, assinarNewsletter, dataNascimento, estado, cidade, sobre)
     {
+    }
+
+    public static Adotante Create(Guid id, Guid tenantId, string nome, string apelido, string documento, string site, Contato contato, bool telefoneVisivel, bool assinarNewsletter, DateTime dataNascimento, string estado, string cidade, string? sobre)
+    {
+        return new Adotante(
+            id,
+            tenantId,
+            nome,
+            apelido,
+            documento,
+            site,
+            contato,
+            telefoneVisivel,
+            assinarNewsletter,
+            dataNascimento,
+            estado,
+            cidade,
+            sobre);
+    }
+
+    public static Adotante Create(Guid id)
+    {
+        return new Adotante(id);
+    }
+
+    public void Update(string nome, string apelido, string site, Contato contato, bool telefoneVisivel, string estado, string cidade, string? sobre)
+    {
+        var errors = new List<string>();
+
+        // Validações
+        if (string.IsNullOrWhiteSpace(nome))
+            errors.Add("Nome não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(apelido))
+            errors.Add("Apelido não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(site))
+            errors.Add("Site não pode ser nulo ou vazio.");
+        if (contato == null)
+            errors.Add("Contato deve conter telefone ou email.");
+        if (string.IsNullOrWhiteSpace(estado))
+            errors.Add("Estado não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(cidade))
+            errors.Add("Cidade não pode ser nula ou vazia.");
+
+        if (errors.Count != 0)
+            throw new DomainException(string.Join(Environment.NewLine, errors));
+
+        Nome = nome;
+        Apelido = apelido;
+        Site = site;
+        Contato = contato!;
+        TelefoneVisivel = telefoneVisivel;
+        Estado = estado;
+        Cidade = cidade;
+        Sobre = sobre;
     }
 }
--- src/services/SGONGA.WebAPI.Business/Models/ONG.cs
+++ src/services/SGONGA.WebAPI.Business/Models/ONG.cs
@@ -7,13 +7,68 @@ public sealed class ONG : Usuario
     public string? ChavePix { get; private set; } = string.Empty;
     public List<Animal> Animais { get; private set; } = new();
 
-    public ONG() { }
+    private ONG() { }
+    private ONG(Guid id) : base(id) { }
 
-    public ONG(Guid id, Guid tenantId, string nome, string apelido, string documento, string site, Contato contato, bool telefoneVisivel, bool assinarNewsletter, DateTime dataNascimento, string estado, string cidade, string? sobre, string? chavePix) : base(id, tenantId, EUsuarioTipo.ONG, nome, apelido, documento, site, contato, telefoneVisivel, assinarNewsletter, dataNascimento, estado, cidade, sobre)
+    private ONG(Guid id, Guid tenantId, string nome, string apelido, string documento, string site, Contato contato, bool telefoneVisivel, bool assinarNewsletter, DateTime dataNascimento, string estado, string cidade, string? sobre, string? chavePix) : base(id, tenantId, EUsuarioTipo.ONG, nome, apelido, documento, site, contato, telefoneVisivel, assinarNewsletter, dataNascimento, estado, cidade, sobre)
     {
         ChavePix = string.IsNullOrEmpty(chavePix) ? documento : chavePix;
     }
+    public static ONG Create(Guid id, Guid tenantId, string nome, string apelido, string documento, string site, Contato contato, bool telefoneVisivel, bool assinarNewsletter, DateTime dataNascimento, string estado, string cidade, string? sobre, string? chavePix)
+    {
+        return new ONG(
+            id,
+            tenantId,
+            nome,
+            apelido,
+            documento,
+            site,
+            contato,
+            telefoneVisivel,
+            assinarNewsletter,
+            dataNascimento,
+            estado,
+            cidade,
+            sobre,
+            chavePix);
+    }
 
+    public static ONG Create(Guid id)
+    {
+        return new ONG(id);
+    }
+
+    public void Update(string nome, string apelido, string site, Contato contato, bool telefoneVisivel, string estado, string cidade, string? sobre, string? chavePix)
+    {
+        var errors = new List<string>();
+
+        // Validações
+        if (string.IsNullOrWhiteSpace(nome))
+            errors.Add("Nome não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(apelido))
+            errors.Add("Apelido não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(site))
+            errors.Add("Site não pode ser nulo ou vazio.");
+        if (contato == null)
+            errors.Add("Contato deve conter telefone ou email.");
+        if (string.IsNullOrWhiteSpace(estado))
+            errors.Add("Estado não pode ser nulo ou vazio.");
+        if (string.IsNullOrWhiteSpace(cidade))
+            errors.Add("Cidade não pode ser nula ou vazia.");
+
+        if (errors.Count != 0)
+            throw new DomainException(string.Join(Environment.NewLine, errors));
+
+        Nome = nome;
+        Apelido = apelido;
+        Site = site;
+        Contato = contato!;
+        TelefoneVisivel = telefoneVisivel;
+        Estado = estado;
+        Cidade = cidade;
+        Sobre = sobre;
+        ChavePix = chavePix;
+    }
     public void AddAnimal(Animal animal)
     {
         ArgumentNullException.ThrowIfNull(animal);
