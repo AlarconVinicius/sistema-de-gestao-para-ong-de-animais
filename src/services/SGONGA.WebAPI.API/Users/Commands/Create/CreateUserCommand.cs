26eeed68d20e33b0081f03e8da4e9565127aaa95
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:32:53 -0300

    feat: Adicionar CreateUserCommandHandler
    #18

---
+++ src/services/SGONGA.WebAPI.API/Users/Commands/Create/CreateUserCommand.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Requests.Shared;
+
+namespace SGONGA.WebAPI.API.Users.Commands.Create;
+
+public record CreateUserCommand(
+    EUsuarioTipo UsuarioTipo,
+    string Nome,
+    string Apelido,
+    string Documento,
+    string Site,
+    ContatoRequest Contato,
+    string Senha,
+    string ConfirmarSenha,
+    bool TelefoneVisivel,
+    bool AssinarNewsletter,
+    DateTime DataNascimento,
+    string Estado,
+    string Cidade,
+    string? Sobre,
+    string? ChavePix) : BaseUserCommand(UsuarioTipo, Nome, Apelido, Documento, Site, Contato, TelefoneVisivel, AssinarNewsletter, DataNascimento, Estado, Cidade, Sobre, ChavePix)
+{
+    public override Result IsValid()
+    {
+        ValidationResult = new CreateUserCommandValidator().Validate(this);
+        if (!ValidationResult.IsValid)
+        {
+            return ValidationResult.Errors.Select(x => Error.Validation(x.ErrorMessage)).ToList();
+        }
+
+        return Result.Ok();
+    }
+
+    public class CreateUserCommandValidator : BaseUserCommandValidator<CreateUserCommand>
+    {
+        public const string PasswordRequired = "A senha é obrigatória.";
+        public const string PasswordConfirmMismatch = "As senhas não conferem.";
+        public const string MinMaxLength = "O campo deve ter entre {0} e {1} caracteres.";
+
+        public CreateUserCommandValidator()
+        {
+            RuleFor(c => c.Senha)
+                .NotEmpty().WithMessage(PasswordRequired)
+                .Length(6, 100).WithMessage(MinMaxLength);
+
+            RuleFor(c => c.ConfirmarSenha)
+                .Equal(c => c.Senha).WithMessage(PasswordConfirmMismatch);
+        }
+    }
+}
---
+++ src/services/SGONGA.WebAPI.API/Users/Commands/Create/CreateUserCommandHandler.cs
@@ -0,0 +1,122 @@
+﻿using SGONGA.Core.Extensions;
+using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.Errors;
+using SGONGA.WebAPI.Business.Interfaces.Handlers;
+using SGONGA.WebAPI.Business.Interfaces.Repositories;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Models.DomainObjects;
+using SGONGA.WebAPI.Business.Requests;
+using SGONGA.WebAPI.Business.Responses;
+using SGONGA.WebAPI.Business.Users.Interfaces.Repositories;
+
+namespace SGONGA.WebAPI.API.Users.Commands.Create;
+
+public class CreateUserCommandHandler(IGenericUnitOfWork UnitOfWork, IUserRepository UserRepository, IIdentityHandler IdentityHandler) : ICommandHandler<CreateUserCommand>
+{
+    public async Task<Result> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+    {
+
+        Result validationResult = command.IsValid();
+        if (validationResult.IsFailed)
+            return validationResult.Errors;
+
+        Result available = await CheckAvailabilityAsync(command);
+        if (available.IsFailed)
+            return available.Errors;
+
+        Result identityResult;
+        Guid newUserId = Guid.NewGuid();
+        Guid newTenantId = Guid.NewGuid();
+        switch (command.UsuarioTipo)
+        {
+            case EUsuarioTipo.Adotante:
+                await UnitOfWork.InsertAsync(
+                    Adotante.Create(
+                        newUserId,
+                        newTenantId,
+                        command.Nome,
+                        command.Apelido,
+                        command.Documento,
+                        command.Site,
+                        new Contato(
+                            command.Contato.Telefone,
+                            command.Contato.Email),
+                        command.TelefoneVisivel,
+                        command.AssinarNewsletter,
+                        command.DataNascimento,
+                        command.Estado,
+                        command.Cidade,
+                        command.Sobre),
+                    cancellationToken);
+                break;
+
+            case EUsuarioTipo.ONG:
+                await UnitOfWork.InsertAsync(
+                    ONG.Create(
+                        newUserId,
+                        newTenantId,
+                        command.Nome,
+                        command.Apelido,
+                        command.Documento,
+                        command.Site,
+                        new Contato(
+                            command.Contato.Telefone,
+                            command.Contato.Email),
+                        command.TelefoneVisivel,
+                        command.AssinarNewsletter,
+                        command.DataNascimento,
+                        command.Estado,
+                        command.Cidade,
+                        command.Sobre,
+                        command.ChavePix),
+                    cancellationToken);
+                break;
+
+            default:
+                return UsuarioErrors.NaoFoiPossivelCriarUsuario;
+        }
+
+        identityResult = await IdentityHandler.CreateAsync(new CreateUserRequest(newUserId, command.Contato.Email, command.Senha, command.ConfirmarSenha));
+        if (identityResult.IsFailed)
+            return identityResult.Errors;
+
+        await IdentityHandler.AddOrUpdateUserClaimAsync(new AddOrUpdateUserClaimRequest(newUserId, new UserClaim("Tenant", newTenantId.ToString())));
+
+        await UnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Ok();
+    }
+    private async Task<Result> CheckAvailabilityAsync(CreateUserCommand command)
+    {
+        if ((await DocumentoDisponivel(command.Documento, command.UsuarioTipo)).IsFailed)
+            return ValidationErrors.DocumentoEmUso(command.Documento);
+
+        if ((await ApelidoDisponivel(command.Apelido, command.UsuarioTipo)).IsFailed)
+            return ValidationErrors.ApelidoEmUso(command.Apelido);
+
+        if ((await EmailDisponivel(command.Contato.Email, command.UsuarioTipo)).IsFailed)
+            return ValidationErrors.EmailEmUso(command.Contato.Email);
+
+        return Result.Ok();
+    }
+
+    private async Task<Result> ApelidoDisponivel(string apelido, EUsuarioTipo tipo)
+    {
+        var available = !await UserRepository.ExistsAsync(f => (f.Apelido == apelido || f.Slug == apelido.SlugifyString()) && f.UsuarioTipo == tipo);
+
+        return available ? Result.Ok() : ValidationErrors.ApelidoEmUso(apelido);
+    }
+    private async Task<Result> DocumentoDisponivel(string documento, EUsuarioTipo tipo)
+    {
+        var available = !await UserRepository.ExistsAsync(f => f.Documento == documento && f.UsuarioTipo == tipo);
+
+        return available ? Result.Ok() : ValidationErrors.DocumentoEmUso(documento);
+    }
+    private async Task<Result> EmailDisponivel(string email, EUsuarioTipo tipo)
+    {
+        var available = !await UserRepository.ExistsAsync(f => f.Contato.Email.Endereco == email && f.UsuarioTipo == tipo);
+
+        return available ? Result.Ok() : ValidationErrors.EmailEmUso(email);
+    }
+}
