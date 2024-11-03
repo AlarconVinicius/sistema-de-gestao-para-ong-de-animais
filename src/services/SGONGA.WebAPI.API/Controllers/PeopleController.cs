e2a1e270fc14ed2062cb14fb974ae81bd2ca4d10
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:36:10 -0300

    refactor: Atualizar UsuariosController para o uso do MediatR e renomear para PeopleController
    #18

--- src/services/SGONGA.WebAPI.API/Controllers/PeopleController.cs
+++ src/services/SGONGA.WebAPI.API/Controllers/PeopleController.cs
@@ -5,23 +5,22 @@ using SGONGA.Core.Configurations;
 using SGONGA.Core.Notifications;
 using SGONGA.WebAPI.API.Controllers.Shared;
 using SGONGA.WebAPI.API.Extensions;
+using SGONGA.WebAPI.API.Users.Commands.Create;
+using SGONGA.WebAPI.API.Users.Commands.Delete;
+using SGONGA.WebAPI.API.Users.Commands.Update;
+using SGONGA.WebAPI.API.Users.Queries.GetAll;
+using SGONGA.WebAPI.API.Users.Queries.GetById;
 using SGONGA.WebAPI.Business.Abstractions;
-using SGONGA.WebAPI.Business.Interfaces.Handlers;
-using SGONGA.WebAPI.Business.Requests;
-using SGONGA.WebAPI.Business.Responses;
+using SGONGA.WebAPI.Business.Animals.Responses;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Users.Responses;
 
 namespace SGONGA.WebAPI.API.Controllers;
 
 [Authorize]
 [Route("api/v1/usuarios/admin/")]
-public class UsuariosController : ApiController
+public class PeopleController(INotifier notifier, ISender sender) : ApiController(notifier, sender)
 {
-    public readonly IUsuarioHandler _usuarioHandler;
-    public UsuariosController(INotifier notifier, IUsuarioHandler usuarioHandler, ISender sender) : base(notifier, sender)
-    {
-        _usuarioHandler = usuarioHandler;
-    }
-
     #region Public Methods
 
     /// <summary>
@@ -31,7 +30,8 @@ public class UsuariosController : ApiController
     /// Este endpoint permite o cadastro de um novo usuario. 
     /// Não é necessário fornecer cabeçalhos de autenticação ou identificação de locatário para este endpoint.
     /// </remarks>
-    /// <param name="request">Dados de cadastro do usuario</param>
+    /// <param name="command">Dados de cadastro do usuario</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Resposta indicando o sucesso ou falha na criação do usuario</returns>
     /// <response code="201">Usuario criado com sucesso</response>
     /// <response code="400">Retorna erros de validação</response>
@@ -39,13 +39,9 @@ public class UsuariosController : ApiController
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpPost("/api/v1/usuarios")]
-    public async Task<IResult> Post(CreateUsuarioRequest request)
+    public async Task<IResult> Post(CreateUserCommand command, CancellationToken cancellationToken)
     {
-        if (!ModelState.IsValid)
-        {
-            return ModelState.ToProblemDetails();
-        }
-        var result = await _usuarioHandler.CreateAsync(request);
+        var result = await Sender.Send(command, cancellationToken);
 
         return result.Match(
             onSuccess: () => Results.Created(),
@@ -60,17 +56,19 @@ public class UsuariosController : ApiController
     /// Não é necessário fornecer cabeçalhos de autenticação ou identificação de locatário para este endpoint.
     /// </remarks>
     /// <param name="id">ID do usuario (GUID)</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Detalhes do usuario</returns>
     /// <response code="200">Detalhes do usuario retornados com sucesso</response>
     /// <response code="400">Retorna erros relacionados à requisição</response>
     [AllowAnonymous]
-    [ProducesResponseType(typeof(UsuarioResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(PersonResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpGet("/api/v1/usuarios/{id:guid}")]
-    public async Task<IResult> GetByIdPublic(Guid id)
+    public async Task<IResult> GetByIdPublic(Guid id, CancellationToken cancellationToken = default)
     {
-        GetUsuarioByIdRequest request = new(id, false);
-        var result = await _usuarioHandler.GetByIdAsync(request);
+        GetUserByIdQuery query = new(id, false);
+
+        var result = await Sender.Send(query, cancellationToken);
 
         return result.Match(
             onSuccess: response => Results.Ok(response),
@@ -88,23 +86,49 @@ public class UsuariosController : ApiController
     /// <param name="page">Número da página para paginação (padrão: 1)</param>
     /// <param name="q">Consulta para filtragem de usuarios</param>
     /// <param name="tipo">Tipo de usuário a ser buscado, 0 para Adotante e 1 para ONG</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Lista de usuarios</returns>
     /// <response code="200">Lista de usuarios retornada com sucesso</response>
     /// <response code="400">Retorna erros relacionados à requisição</response>
     [AllowAnonymous]
-    [ProducesResponseType(typeof(PagedResponse<UsuarioResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BasePagedResponse<PersonResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [HttpGet("/api/v1/usuarios")]
-    public async Task<IResult> GetAllPublic([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, [FromQuery] int tipo = 0)
+    [HttpGet("/api/v1/usuarios/adotantes")]
+    public async Task<IResult> GetAllAdoptersPublic([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, [FromQuery] int tipo = 0, CancellationToken cancellationToken = default)
     {
-        GetAllUsuariosRequest request = new(
-            pageSize: ps,
-            pageNumber: page,
-            query: q,
-            tenantFiltro: false,
-            usuarioTipo: tipo
-        );
-        var result = await _usuarioHandler.GetAllAsync(request);
+        GetAllUsersQuery query = new(EUsuarioTipo.Adotante, ps, page, q, false, false);
+
+        var result = await Sender.Send(query, cancellationToken);
+
+        return result.Match(
+            onSuccess: response => Results.Ok(response),
+            onFailure: response => response.ToProblemDetails());
+    }
+
+    /// <summary>
+    /// Retorna todos os usuarios públicos.
+    /// </summary>
+    /// <remarks>
+    /// Este endpoint permite recuperar uma lista de todos os usuarios de forma pública.
+    /// Não é necessário fornecer cabeçalhos de autenticação ou identificação de locatário para este endpoint.
+    /// </remarks>
+    /// <param name="ps">Tamanho da página para paginação (padrão: 25)</param>
+    /// <param name="page">Número da página para paginação (padrão: 1)</param>
+    /// <param name="q">Consulta para filtragem de usuarios</param>
+    /// <param name="tipo">Tipo de usuário a ser buscado, 0 para Adotante e 1 para ONG</param>
+    /// <param name="cancellationToken">Ignored</param>
+    /// <returns>Lista de usuarios</returns>
+    /// <response code="200">Lista de usuarios retornada com sucesso</response>
+    /// <response code="400">Retorna erros relacionados à requisição</response>
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(BasePagedResponse<PersonResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [HttpGet("/api/v1/usuarios/ongs")]
+    public async Task<IResult> GetAllNGOsPublic([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, [FromQuery] int tipo = 0, CancellationToken cancellationToken = default)
+    {
+        GetAllUsersQuery query = new(EUsuarioTipo.ONG, ps, page, q, false, false);
+
+        var result = await Sender.Send(query, cancellationToken);
 
         return result.Match(
             onSuccess: response => Results.Ok(response),
@@ -128,20 +152,22 @@ public class UsuariosController : ApiController
     /// O `TenantId` deve ser o identificador do locatário que está fazendo a requisição.
     /// </remarks>
     /// <param name="id">ID do usuario (GUID)</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Detalhes do usuario</returns>
     /// <response code="200">Detalhes do usuario retornados com sucesso</response>
     /// <response code="400">Retorna erros relacionados à requisição</response>
     /// <response code="401">Usuário não autorizado. Token de autenticação ausente ou inválido</response>
     /// <response code="403">Permissão negada. Usuário não possui privilégios para acessar este recurso</response>
-    [ProducesResponseType(typeof(UsuarioResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(PersonResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpGet("{id:guid}")]
-    public async Task<IResult> GetById(Guid id)
+    public async Task<IResult> GetById(Guid id, CancellationToken cancellationToken)
     {
-        GetUsuarioByIdRequest request = new(id, true);
-        var result = await _usuarioHandler.GetByIdAsync(request);
+        GetUserByIdQuery query = new(id, true);
+
+        var result = await Sender.Send(query, cancellationToken);
 
         return result.Match(
             onSuccess: response => Results.Ok(response),
@@ -165,26 +191,22 @@ public class UsuariosController : ApiController
     /// <param name="page">Número da página para paginação (padrão: 1)</param>
     /// <param name="q">Consulta para filtragem de usuarios</param>
     /// <param name="tipo">Tipo de usuário a ser buscado, 0 para Adotante e 1 para ONG</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Lista de usuarios</returns>
     /// <response code="200">Lista de usuarios retornada com sucesso</response>
     /// <response code="400">Retorna erros relacionados à requisição</response>
     /// <response code="401">Usuário não autorizado. Token de autenticação ausente ou inválido</response>
     /// <response code="403">Permissão negada. Usuário não possui privilégios para acessar este recurso</response>
-    [ProducesResponseType(typeof(PagedResponse<UsuarioResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BasePagedResponse<PersonResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpGet]
-    public async Task<IResult> GetAll([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, [FromQuery] int tipo = 0)
+    public async Task<IResult> GetAllNGO([FromQuery] int ps = ConfigurationDefault.DefaultPageSize, [FromQuery] int page = ConfigurationDefault.DefaultPageNumber, [FromQuery] string q = null!, [FromQuery] int tipo = 0, CancellationToken cancellationToken = default)
     {
-        GetAllUsuariosRequest request = new(
-            pageSize: ps,
-            pageNumber: page,
-            query: q,
-            tenantFiltro: true,
-            usuarioTipo: tipo
-        );
-        var result = await _usuarioHandler.GetAllAsync(request);
+        GetAllUsersQuery query = new(EUsuarioTipo.ONG, ps, page, q, false, true);
+
+        var result = await Sender.Send(query, cancellationToken);
 
         return result.Match(
             onSuccess: response => Results.Ok(response),
@@ -206,6 +228,7 @@ public class UsuariosController : ApiController
     /// </remarks>
     /// <param name="id">ID do usuario (GUID)</param>
     /// <param name="request">Dados atualizados do usuario</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Resposta indicando o sucesso ou falha na atualização do usuario</returns>
     /// <response code="204">Usuario atualizado com sucesso</response>
     /// <response code="400">Retorna erros de validação ou IDs não correspondentes</response>
@@ -216,17 +239,11 @@ public class UsuariosController : ApiController
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpPut("{id:guid}")]
-    public async Task<IResult> Update(Guid id, UpdateUsuarioRequest request)
+    public async Task<IResult> Update(Guid id, UpdateUserRequest request, CancellationToken cancellationToken = default)
     {
-        if (id != request.Id)
-            ModelState.AddModelError(string.Empty, "Os IDs não correspondem.");
+        UpdateUserCommand command = new(id, request.UsuarioTipo, request.Nome, request.Apelido, request.Documento, request.Site, new(request.Contato.Telefone, request.Contato.Email), request.TelefoneVisivel, request.AssinarNewsletter, request.DataNascimento, request.Estado, request.Estado, request.Sobre, request.ChavePix);
 
-        if (!ModelState.IsValid)
-        {
-            return ModelState.ToProblemDetails();
-        }
-
-        var result = await _usuarioHandler.UpdateAsync(request);
+        var result = await Sender.Send(command, cancellationToken);
 
         return result.Match(
             onSuccess: () => Results.NoContent(),
@@ -247,6 +264,7 @@ public class UsuariosController : ApiController
     /// O `TenantId` deve ser o identificador do locatário que está fazendo a requisição.
     /// </remarks>
     /// <param name="id">ID do usuario (GUID)</param>
+    /// <param name="cancellationToken">Ignored</param>
     /// <returns>Resposta indicando o sucesso ou falha na exclusão do usuario</returns>
     /// <response code="204">Usuario deletado com sucesso</response>
     /// <response code="400">Retorna erros relacionados à requisição</response>
@@ -257,9 +275,11 @@ public class UsuariosController : ApiController
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpDelete("{id:guid}")]
-    public async Task<IResult> Delete(Guid id)
+    public async Task<IResult> Delete(Guid id, CancellationToken cancellationToken = default)
     {
-        var result = await _usuarioHandler.DeleteAsync(new DeleteUsuarioRequest(id));
+        DeleteUserCommand command = new(id);
+
+        var result = await Sender.Send(command, cancellationToken);
 
         return result.Match(
             onSuccess: () => Results.NoContent(),
