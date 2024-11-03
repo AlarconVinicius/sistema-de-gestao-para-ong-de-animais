b57c08292af131187809054ca67eddc1b060bbd6
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-03 13:05:25 -0300

    feat: Adicionar JsonIgnore à propriedade ValidationResult
    #18

--- src/services/SGONGA.WebAPI.API/Abstractions/Messaging/ICommand.cs
+++ src/services/SGONGA.WebAPI.API/Abstractions/Messaging/ICommand.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.Results;
 using MediatR;
 using SGONGA.WebAPI.Business.Abstractions;
+using System.Text.Json.Serialization;
 
 namespace SGONGA.WebAPI.API.Abstractions.Messaging;
 
@@ -13,6 +14,7 @@ public interface ICommand<TResponse> : IRequest<Result<TResponse>>
 }
 public abstract record BaseCommand : ICommand
 {
+    [JsonIgnore]
     public ValidationResult ValidationResult { get; set; } = new ValidationResult();
 
     public abstract Result IsValid();
