887f6ba9859d5eb40619489c8ba527d665cbe20c
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:33:34 -0300

    feat: Adicionar BaseUserCommandHandler
    #18

---
+++ src/services/SGONGA.WebAPI.API/Users/Commands/BaseUserCommand.cs
@@ -0,0 +1,83 @@
+﻿using FluentValidation;
+using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Requests.Shared;
+using static SGONGA.WebAPI.Business.Requests.Shared.ContatoRequest;
+
+namespace SGONGA.WebAPI.API.Users.Commands;
+
+public abstract record BaseUserCommand(
+        EUsuarioTipo UsuarioTipo,
+        string Nome,
+        string Apelido,
+        string Documento,
+        string Site,
+        ContatoRequest Contato,
+        bool TelefoneVisivel,
+        bool AssinarNewsletter,
+        DateTime DataNascimento,
+        string Estado,
+        string Cidade,
+        string? Sobre,
+        string? ChavePix) : BaseCommand
+{
+    public class BaseUserCommandValidator<T> : AbstractValidator<T> where T : BaseUserCommand
+    {
+
+        public const string NameRequired = "O nome é obrigatório.";
+        public const string NicknameRequired = "O apelido é obrigatório.";
+        public const string DocumentRequired = "O documento é obrigatório.";
+        public const string SiteRequired = "O site é obrigatório.";
+        public const string BirthdateRequired = "A data de nascimento é obrigatória.";
+        public const string StateRequired = "O estado é obrigatório.";
+        public const string CityRequired = "A cidade é obrigatória.";
+        public const string MaxLengthExceeded = "O campo deve ter no máximo {0} caracteres.";
+        public const string InvalidUrl = "URL inválida.";
+        public const string UserTypeRequired = "O tipo de usuário é obrigatório.";
+        public const string ContactRequired = "As informações de contato são obrigatórias.";
+
+        public BaseUserCommandValidator()
+        {
+            RuleFor(c => c.UsuarioTipo)
+                .IsInEnum().WithMessage(UserTypeRequired);
+
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage(NameRequired)
+                .MaximumLength(100).WithMessage(MaxLengthExceeded);
+
+            RuleFor(c => c.Apelido)
+                .NotEmpty().WithMessage(NicknameRequired)
+                .MaximumLength(100).WithMessage(MaxLengthExceeded);
+
+            RuleFor(c => c.Documento)
+                .NotEmpty().WithMessage(DocumentRequired)
+                .MaximumLength(11).WithMessage(MaxLengthExceeded);
+
+            RuleFor(c => c.Site)
+                .NotEmpty().WithMessage(SiteRequired)
+                .MaximumLength(500).WithMessage(MaxLengthExceeded)
+                .Must(BeAValidUrl).WithMessage(InvalidUrl);
+
+            RuleFor(c => c.DataNascimento)
+                .NotEmpty().WithMessage(BirthdateRequired);
+
+            RuleFor(c => c.Estado)
+                .NotEmpty().WithMessage(StateRequired)
+                .MaximumLength(50).WithMessage(MaxLengthExceeded);
+
+            RuleFor(c => c.Cidade)
+                .NotEmpty().WithMessage(CityRequired)
+                .MaximumLength(50).WithMessage(MaxLengthExceeded);
+
+            RuleFor(c => c.Contato)
+            .NotNull().WithMessage(ContactRequired)
+            .SetValidator(new ContatoRequestValidator());
+        }
+
+        private bool BeAValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
