b1bbfc3f5ee5dc36efb6845bb2169a5463df8790
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:33:59 -0300

    feat: Adicionar GetAllUsersQueryHandler
    #18

---
+++ src/services/SGONGA.WebAPI.API/Users/Queries/GetAll/GetAllUsersQuery.cs
@@ -0,0 +1,19 @@
+﻿using SGONGA.WebAPI.API.Shared;
+using SGONGA.WebAPI.Business.Animals.Responses;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Users.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Queries.GetAll;
+
+public class GetAllUsersQuery : BasePagedQuery<BasePagedResponse<PersonResponse>>
+{
+    public EUsuarioTipo UsuarioTipo { get; }
+    public bool TenantFiltro { get; }
+    public GetAllUsersQuery() { }
+
+    public GetAllUsersQuery(EUsuarioTipo usuarioTipo, int pageSize = 50, int pageNumber = 1, string? query = null, bool returnAll = false, bool tenantFiltro = false) : base(pageSize, pageNumber, query, returnAll)
+    {
+        UsuarioTipo = usuarioTipo;
+        TenantFiltro = tenantFiltro;
+    }
+}
---
+++ src/services/SGONGA.WebAPI.API/Users/Queries/GetAll/GetAllUsersQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.Animals.Responses;
+using SGONGA.WebAPI.Business.Errors;
+using SGONGA.WebAPI.Business.Interfaces.Services;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Users.Interfaces.Repositories;
+using SGONGA.WebAPI.Business.Users.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Queries.GetAll;
+
+public class GetAllUsersQueryHandler(IUserRepository UserRepository, ITenantProvider TenantProvider) : IQueryHandler<GetAllUsersQuery, BasePagedResponse<PersonResponse>>
+{
+    public async Task<Result<BasePagedResponse<PersonResponse>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        Guid? tenantId = null;
+        if (request.TenantFiltro)
+        {
+            Result<Guid> tenantResult = await TenantProvider.GetTenantId();
+            if (tenantResult.IsFailed)
+                return tenantResult.Errors;
+            tenantId = tenantResult.Value;
+        }
+        switch (request.UsuarioTipo)
+        {
+            case EUsuarioTipo.Adotante:
+                return await UserRepository.GetAllAdoptersPagedAsync(tenantId, request.PageNumber, request.PageSize, request.Query, request.ReturnAll, cancellationToken);
+
+            case EUsuarioTipo.ONG:
+                return await UserRepository.GetAllNGOsPagedAsync(tenantId, request.PageNumber, request.PageSize, request.Query, request.ReturnAll, cancellationToken);
+            default:
+                return UsuarioErrors.NaoFoiPossivelRecuperarUsuarios;
+        }
+    }
+}
