d8a9065c1256c0f457a75c0599a93b16d206c43b
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-11-03 10:34:25 -0300

    feat: Adicionar GetUserByIdQueryHandler
    #18

---
+++ src/services/SGONGA.WebAPI.API/Users/Queries/GetById/GetUserByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿
+using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Users.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Queries.GetById;
+
+public record GetUserByIdQuery(Guid Id, bool TenantFiltro = false) : IQuery<PersonResponse>;
---
+++ src/services/SGONGA.WebAPI.API/Users/Queries/GetById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.Errors;
+using SGONGA.WebAPI.Business.Interfaces.Services;
+using SGONGA.WebAPI.Business.Models;
+using SGONGA.WebAPI.Business.Users.Interfaces.Repositories;
+using SGONGA.WebAPI.Business.Users.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Queries.GetById;
+
+public class GetUserByIdQueryHandler(IUserRepository UserRepository, ITenantProvider TenantProvider) : IQueryHandler<GetUserByIdQuery, PersonResponse>
+{
+    public async Task<Result<PersonResponse>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        Guid? tenantId = null;
+        if (request.TenantFiltro)
+        {
+            Result<Guid> tenantResult = await TenantProvider.GetTenantId();
+            if (tenantResult.IsFailed)
+                return tenantResult.Errors;
+            tenantId = tenantResult.Value;
+        }
+
+        var userType = await UserRepository.IdentifyUserType(request.Id, tenantId, cancellationToken);
+        if (userType.IsFailed)
+            return userType.Errors;
+
+        switch (userType.Value)
+        {
+            case EUsuarioTipo.Adotante:
+                return await UserRepository.GetAdopterByIdAsync(request.Id, tenantId, cancellationToken);
+
+            case EUsuarioTipo.ONG:
+                return await UserRepository.GetNGOByIdAsync(request.Id, tenantId, cancellationToken);
+
+            default:
+                return UsuarioErrors.NaoFoiPossivelRecuperarUsuario;
+        }
+    }
+}
