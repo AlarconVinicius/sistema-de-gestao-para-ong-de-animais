a0f81bab46754ce87c08e7fc9f5fcfcced71445a
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-09 09:39:09 -0300

    feat: Add env configurations to appsettings

--- src/services/SGONGA.WebAPI.API/Program.cs
+++ src/services/SGONGA.WebAPI.API/Program.cs
@@ -8,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddApiConfig();
 
 builder.Services.RegisterApiServices()
