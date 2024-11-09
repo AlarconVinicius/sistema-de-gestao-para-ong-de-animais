9e26f14a2ebafe6ff0634bfa84dc134d4ebfa361
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-09 10:26:34 -0300

    test: Implement SlugTests
    #22

---
+++ src/tests/SGONGA.WebAPI.Business.Tests/People/ValueObjects/SlugTests.cs
@@ -0,0 +1,55 @@
+﻿using SGONGA.WebAPI.Business.People.Exceptions;
+using SGONGA.WebAPI.Business.People.ValueObjects;
+
+namespace SGONGA.WebAPI.Business.Tests.People.ValueObjects;
+
+[Trait("Person", "ValueObjects - Organization")]
+public class SlugTests
+{
+
+    [Fact]
+    public void Constructor_Should_ReturnSlug_WhenCalledWithValidSlug()
+    {
+        // Arrange
+        string validSlug = "Slug válida";
+
+        // Act
+        Slug slug = validSlug;
+
+        // Assert
+        Assert.Equal("slug-valida", slug);
+        Assert.True(slug.UrlPath.Length < Slug.MaxLength);
+        Assert.True(slug.UrlPath.Length > Slug.MinLength);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowException_WhenCalledWithInvalidSlug()
+    {
+        // Arrange
+        string invalidSlug = "";
+
+        // Act & Assert
+        var exception = Assert.Throws<PersonValidationException>(() =>
+        {
+            Slug slug = invalidSlug;
+        });
+
+        Assert.Equal("Slug inválida", exception.Errors.First().Message);
+        Assert.Single(exception.Errors);
+    }
+
+    [Theory]
+    [InlineData("sl")]
+    [InlineData("slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug slug")]
+    public void Constructor_ShouldThrowException_WhenCalledWithInvalidLength(string invalidSlug)
+    {
+        // Arrange & Act & Assert
+        var exception = Assert.Throws<PersonValidationException>(() =>
+        {
+            Slug slug = invalidSlug;
+        });
+
+        Assert.Equal("Slug deve conter de 3 à 100 caracteres", exception.Errors.First().Message);
+        Assert.Single(exception.Errors);
+    }
+}
