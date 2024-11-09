ffab998c041e18c378afb0a059822e113c0d60a8
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-09 11:52:10 -0300

    test: Implement EmailTests
    #22

---
+++ src/tests/SGONGA.WebAPI.Business.Tests/People/ValueObjects/EmailTests.cs
@@ -0,0 +1,59 @@
+﻿using SGONGA.WebAPI.Business.People.Exceptions;
+using SGONGA.WebAPI.Business.People.ValueObjects;
+
+namespace SGONGA.WebAPI.Business.Tests.People.ValueObjects;
+
+[Trait("Person", "ValueObjects - Email")]
+public class EmailTests
+{
+    [Fact]
+    public void Constructor_Should_Initialize_WhenCalledWithValidEmails()
+    {
+        // Arrange
+        string validEmail = "email@email.com";
+        // Act
+        Email email = validEmail;
+
+        // Assert
+        Assert.Equal(validEmail, email.Address);
+        Assert.True(email.Address.Length < Email.MaxLength);
+        Assert.True(email.Address.Length > Email.MinLength);
+    }
+
+    [Theory]
+    [InlineData("email@")]
+    [InlineData("email@domain")]
+    [InlineData("email@domain.c")]
+    [InlineData("email!@domain.com")]
+    [InlineData("email@domain#.com")]
+    [InlineData("e..mail@domain.com")]
+    [InlineData("email@com")]
+    [InlineData("emal@(domain.com)")]
+    public void Constructor_Should_ThrowException_WhenCalledWithVInvalidEmails(string invalidEmail)
+    {
+        // Arrange & Act & Assert
+        var exception = Assert.Throws<PersonValidationException>(() =>
+        {
+            Email email = invalidEmail;
+        });
+
+        Assert.Contains(exception.Errors, e => e.Message == "Email inválido");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("emai")]
+    [InlineData("email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email email")]
+    public void Constructor_Should_ThrowException_WhenCalledWithInvalidLength(string invalidEmail)
+    {
+        // Arrange & Act & Assert
+        var exception = Assert.Throws<PersonValidationException>(() =>
+        {
+            Email email = invalidEmail;
+        });
+
+        Assert.Contains(exception.Errors, e => e.Message == $"E-mail deve conter entre {Email.MinLength} e {Email.MaxLength} caracteres.");
+    }
+}
