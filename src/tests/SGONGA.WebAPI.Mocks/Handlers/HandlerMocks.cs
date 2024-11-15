34261110c8ce6d7fc4120018cac9b8f3b4ba48bd
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-15 10:13:31 -0300

    test: Add mocks
    #22

---
+++ src/tests/SGONGA.WebAPI.Mocks/Handlers/HandlerMocks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.Language.Flow;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.People.Interfaces.Repositories;
+using SGONGA.WebAPI.Business.Tenants.Interfaces.Handlers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SGONGA.WebAPI.Mocks.Handlers;
+
+[ExcludeFromCodeCoverage]
+public static class HandlerMocks
+{
+    public static Mock<ITenantProvider> TenantProvider() => new();
+    public static Mock<IPersonRepository> PersonRepository() => new();
+}
+
+[ExcludeFromCodeCoverage]
+public static class MockTenantProviderExtension
+{
+    public static ISetup<ITenantProvider, Task<Result<Guid>>> GetTenantId(this Mock<ITenantProvider> mock) =>
+        mock.Setup(m => m.GetTenantId());
+
+    public static ISetup<ITenantProvider, Result> SetTenantId(this Mock<ITenantProvider> mock) =>
+        mock.Setup(m => m.SetTenantId(It.IsAny<IHeaderDictionary>()));
+
+    public static ISetup<ITenantProvider, Result> SetTenantId(this Mock<ITenantProvider> mock, IHeaderDictionary headerDictionary) =>
+        mock.Setup(m => m.SetTenantId(headerDictionary));
+}
---
+++ src/tests/SGONGA.WebAPI.Mocks/Repositories/RepositoryMocks.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using Moq.Language.Flow;
+using SGONGA.WebAPI.Business.People.Entities;
+using SGONGA.WebAPI.Business.People.Interfaces.Repositories;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace SGONGA.WebAPI.Mocks.Repositories;
+
+[ExcludeFromCodeCoverage]
+public static class RepositoryMocks
+{
+    public static Mock<IPersonRepository> PersonRepository() => new();
+}
+
+[ExcludeFromCodeCoverage]
+public static class MockPersonRepositoryExtension
+{
+    public static ISetup<IPersonRepository, Task<bool>> ExistsAsync(this Mock<IPersonRepository> mock) =>
+    mock.Setup(m => m.ExistsAsync(It.IsAny<Expression<Func<Person, bool>>>(), It.IsAny<CancellationToken>()));
+}
