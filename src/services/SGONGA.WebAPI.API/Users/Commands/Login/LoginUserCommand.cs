6b2ed51d8421d1c08fd2e52e2525605fd98ae7d7
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-16 16:30:26 -0300

    feat: Add LoginUserCommand LoginUserCommandHandler
    #23

---
+++ src/services/SGONGA.WebAPI.API/Users/Commands/Login/LoginUserCommand.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Identity.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Commands.Login;
+
+using EmailValueObject = Business.People.ValueObjects.Email;
+public record LoginUserCommand(string Email, string Password) : BaseCommand<LoginUserResponse>
+{
+    public override Result IsValid()
+    {
+        ValidationResult = new LoginUserCommandValidator().Validate(this);
+        if (!ValidationResult.IsValid)
+        {
+            return ValidationResult.Errors.Select(x => Error.Validation(x.ErrorMessage)).ToList();
+        }
+
+        return Result.Ok();
+    }
+
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public const string PasswordRequired = "The password is required.";
+        public const string EmailInvalid = "The email address provided is invalid.";
+        
+        public LoginUserCommandValidator()
+        {
+            RuleFor(c => c.Email)
+                .Must(BeAValidEmail).WithMessage(EmailInvalid);
+
+            RuleFor(c => c.Password)
+                .NotEmpty().WithMessage(PasswordRequired);
+        }
+
+        private bool BeAValidEmail(string email)
+        {
+            try
+            {
+                return EmailValueObject.IsValidEmail(email);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/services/SGONGA.WebAPI.API/Users/Commands/Login/LoginUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using SGONGA.WebAPI.API.Abstractions.Messaging;
+using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Identity.Responses;
+
+namespace SGONGA.WebAPI.API.Users.Commands.Login;
+
+public class LoginUserCommandHandler(Identity.Handlers.IIdentityHandler IdentityHandler) : ICommandHandler<LoginUserCommand, LoginUserResponse>
+{
+    public async Task<Result<LoginUserResponse>> Handle(LoginUserCommand command, CancellationToken cancellationToken)
+    {
+        Result validationResult = command.IsValid();
+        if (validationResult.IsFailed)
+            return validationResult.Errors;
+
+        var result = await IdentityHandler.LoginAsync(new(command.Email, command.Password));
+
+        if (result.IsSuccess)
+            return result.Value;
+
+        return result.Errors;
+    }
+}
