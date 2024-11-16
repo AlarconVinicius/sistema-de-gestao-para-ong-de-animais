87adfc174255f03e33e321e25fcd31ffc0e3d752
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-16 10:55:24 -0300

    test: Implement AnimalErrorsTests
    #22

---
+++ src/tests/SGONGA.WebAPI.Business.Tests/Animals/Errors/AnimalErrorsTests.cs
@@ -0,0 +1,35 @@
+﻿using SGONGA.WebAPI.Business.Abstractions;
+using SGONGA.WebAPI.Business.Animals.Errors;
+
+namespace SGONGA.WebAPI.Business.Tests.Animals.Errors;
+
+[Trait("Animals", "Errors")]
+public class AnimalErrorsTests
+{
+    [Fact]
+    public void AnimalNotFound_ShouldReturnNotFoundError()
+    {
+        // Arrange
+        var animalId = Guid.NewGuid();
+
+        // Act
+        var result = AnimalErrors.AnimalNotFound(animalId);
+
+        // Assert
+        Assert.Equal("ANIMAL_NOT_FOUND", result.Code);
+        Assert.Equal($"The animal with Id = '{animalId}' was not found.", result.Message);
+        Assert.Equal(ErrorType.NotFound, result.Type);
+    }
+
+    [Fact]
+    public void NoAnimalsFound_ShouldReturnNotFoundError()
+    {
+        // Act
+        var result = AnimalErrors.NoAnimalsFound;
+
+        // Assert
+        Assert.Equal("NO_ANIMALS_FOUND", result.Code);
+        Assert.Equal("No animals were found.", result.Message);
+        Assert.Equal(ErrorType.NotFound, result.Type);
+    }
+}
