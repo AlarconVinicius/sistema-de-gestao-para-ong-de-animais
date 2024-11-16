14d0b78474f9837c5a70f4a0c3c01f0c10614d80
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-11-16 16:30:38 -0300

    feat: Add LoginUserCommandExample
    #23

---
+++ src/services/SGONGA.WebAPI.API/Users/Examples/LoginUserExample.cs
@@ -0,0 +1,19 @@
+﻿using SGONGA.WebAPI.API.Users.Commands.Login;
+using Swashbuckle.AspNetCore.Filters;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SGONGA.WebAPI.API.Users.Examples;
+
+[ExcludeFromCodeCoverage]
+public class LoginUserExample : IMultipleExamplesProvider<LoginUserCommand>
+{
+    IEnumerable<SwaggerExample<LoginUserCommand>> IMultipleExamplesProvider<LoginUserCommand>.GetExamples()
+    {
+        yield return SwaggerExample.Create(
+            "Realizar login",
+            new LoginUserCommand(
+            Email: "user@email.com",
+            Password: "P@ssword123"
+           ));
+    }
+}
